7482e81d1739d3877296deefc277e41e2f56fc76
pengJeff <815899718@qq.com>
2025-02-16 17:41:31 +0800

    Update RestfulController.java

--- src/main/java/com/easypeng/restful/controller/RestfulController.java
+++ src/main/java/com/easypeng/restful/controller/RestfulController.java
@@ -1,10 +1,17 @@
 package com.easypeng.restful.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restful")
 public class RestfulController {
+
+
+    // 唯一配置文件中的属性
+
+    @Value("${var.number}")
+    private String number;
 
     @GetMapping
     public String get() {
@@ -25,6 +32,7 @@ public class RestfulController {
     public String patch() {
         return "patch";
     }
+
     @DeleteMapping
     public String delete() {
         return "delete";
